4ebb0a3ef445b50e5b3bfabb137fa81ea28bfb9e
sparda <pak.evgeny@live.ru>
2021-03-25 19:50:20 +0300

    ~ review

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,6 @@
 #include <SPI.h>
-//Add the SdFat Libraries
 #include <SdFat.h>
 #include <SdFatUtil.h>
-//and the MP3 Shield Library
 #include <SFEMP3Shield.h>
 
 // ============================================================
@@ -37,15 +35,12 @@ const double SOUND_VOLUME_MAX = 0;  // input values are -1/2dB. e.g. 40 results 
 const double VIBRO_PWR_MIN = 0;
 const double VIBRO_PWR_MAX = 200;
 
-// const int FRAME_RATE = 300;
-
 const int SENSOR_MAX_VAL = 1023;
 
 // ============================================================
 // VARIABLES
 // ============================================================
 
-// CRGB leds[LEDS_COUNT];
 int led_id_current = 0;
 int led_id_previous = -1;
 int led_color_hue = 0;
@@ -71,7 +66,10 @@ int audio_index = 1;
 SdFat sd;
 SdFile file;
 SFEMP3Shield MP3player;
-uint8_t result;
+
+// ============================================================
+// LED
+// ============================================================
 
 #if defined(MICROLED)
 // ===== ЦВЕТОВАЯ ГЛУБИНА =====
@@ -82,7 +80,8 @@ uint8_t result;
 // без него будет 3 байта по умолчанию
 #define COLOR_DEBTH 3
 #include <microLED.h> // подключаем библу
-microLED<LEDS_COUNT, PIN_LED, -1, LED_WS2812, ORDER_GRB, CLI_AVER> strip;
+// CLI_OFF CLI_LOW CLI_AVER CLI_HIGH
+microLED<LEDS_COUNT, PIN_LED, -1, LED_WS2812, ORDER_GRB, CLI_HIGH> strip;
 
 #else
 #include "FastLED.h"
@@ -170,13 +169,8 @@ void updateIterationProgress()
   time_from_start_prev = time_from_start;
 
   iteration_progress_delta = (double)time_from_last_tick / (60000 / iteration_per_minute);
-  // if (iteration_progress_delta > 1) {
-  //   // iteration_progress_delta -= floor(iteration_progress_delta); // [0, 1]
-  //   iteration_progress_delta = modff(iteration_progress_delta); // [0, 1]
-  // }
 
   iteration_progress += iteration_progress_delta;
-  // iteration_progress -= floor(iteration_progress); // [0, 1]
   iteration_progress = modff(iteration_progress, &foo); // [0, 1)
 }
 
@@ -302,8 +296,7 @@ void proceedSound()
   sound_left_pwr += sound_volume_coef;
   sound_right_pwr += sound_volume_coef;
 
-  // Serial.println(sound_left_pwr);
-  MP3player.setVolume(sound_left_pwr, sound_right_pwr); // commit new volume
+  MP3player.setVolume(sound_left_pwr, sound_right_pwr);
 
   if (MP3player.isPlaying() == 0)
   {
@@ -317,35 +310,8 @@ void proceedSound()
 
 void setup()
 {
-  // This is the list of timers in Arduino Mega 2560
-  // timer 0 (controls pin 13, 4);
-  // timer 1 (controls pin 12, 11);
-  // timer 2 (controls pin 10, 9);
-  // timer 3 (controls pin 5, 3, 2);
-  // timer 4 (controls pin 8, 7, 6);
+  Serial.begin(9600);
 
-  // Pins D9, D10 to 31.4 кГц (Arduino Mega 2560)
-  // use this
-  // TCCR2A = 0b00000001;  // 8bit
-  // TCCR2B = 0b00000001;  // x1 phase correct
-
-  // or use this
-  // int myEraser = 7;             // this is 111 in binary and is used as an eraser
-  // TCCR2B &= ~myEraser;          // this operation (AND plus NOT),  set the three bits in TCCR2B to 0
-
-  // Those prescaler values are good for all timers (TCCR1B, TCCR2B, TCCR3B, TCCR4B) except for timer 0 (TCCR0B).
-  // prescaler = 1 ---> PWM frequency is 31000 Hz
-  // prescaler = 2 ---> PWM frequency is 4000 Hz
-  // prescaler = 3 ---> PWM frequency is 490 Hz (default value)
-  // prescaler = 4 ---> PWM frequency is 120 Hz
-  // prescaler = 5 ---> PWM frequency is 30 Hz
-  // prescaler = 6 ---> PWM frequency is <20 Hz
-  // int myPrescaler = 1;          // this could be a number in [1 , 6].
-  // TCCR2B |= myPrescaler;
-
-  Serial.begin(9600); // set serial output speed
-
-  // set pin as output
   pinMode(PIN_LED, OUTPUT);
   pinMode(PIN_VIBRO_LEFT, OUTPUT);
   pinMode(PIN_VIBRO_RIGHT, OUTPUT);
@@ -378,16 +344,9 @@ void setup()
     }
     file.close();
   }
-  // Serial.println(files_count);
 
-  //Initialize the MP3 Player Shield
-  result = MP3player.begin();
-  // if (result != 0)
-  // {
-  //   Serial.print(F("Error code: "));
-  //   Serial.print(result);
-  //   Serial.println(F(" when trying to start MP3 player"));
-  // }
+  // Initialize Audio
+  MP3player.begin();
 }
 
 void loop()
@@ -403,11 +362,8 @@ void loop()
 #else
   proceedLEDFastLED();
 #endif
-
   // proceedLEDByPosition();
 
   proceedVibro();
   proceedSound();
-
-  // delay(1000.0 / FRAME_RATE); // forced frame rate emulation
 }
